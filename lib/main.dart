82140366600db22528244c932f5c6fcbf341f3ff
Konstantin Kustov <kkustau@scnsoft.com>
2018-12-28 16:25:43 +0300

    code about network was changed

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,27 @@ import 'dart:convert';
 import 'package:pockemon_app/pockemon.dart';
 import 'package:pockemon_app/pockemondetail.dart';
 
-void main() => runApp(MaterialApp(title: "Poke App", home: HomePage()));
+Future<PokeHub> getPokemonRequest() async {
+  final response = await http.get(
+      'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    return PokeHub.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load pokemons');
+  }
+}
+
+void main() => runApp(MaterialApp(
+    title: "Poke App", home: HomePage(pokeHub: getPokemonRequest())));
 
 class HomePage extends StatefulWidget {
+  final Future<PokeHub> pokeHub;
+
+  HomePage({Key key, this.pokeHub}) : super(key: key);
+
   @override
   HomePageState createState() {
     return new HomePageState();
@@ -15,25 +33,6 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  var url =
-      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-
-  PokeHub pokeHub;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  fetchData() async {
-    var res = await http.get(url);
-    var decodedJson = jsonDecode(res.body);
-
-    pokeHub = PokeHub.fromJson(decodedJson);
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,54 +40,63 @@ class HomePageState extends State<HomePage> {
         title: Text("PokeApp"),
         backgroundColor: Colors.cyan,
       ),
-      body: pokeHub == null
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : GridView.count(
-              crossAxisCount: 2,
-              children: pokeHub.pokemon
-                  .map((pokeHub) => Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => PokeDetail(
-                                        pokemon: pokeHub,
-                                      )));
-                        },
-                        child: Hero(
-                          tag: pokeHub.img,
-                          child: Card(
-                            elevation: 3.0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Container(
-                                  height: 100.0,
-                                  width: 100.0,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: NetworkImage(pokeHub.img))),
-                                ),
-                                Text(pokeHub.name,
-                                    style: TextStyle(
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                          ),
+      body: OrientationBuilder(builder: (context, orientation) {
+        return Center(
+          child: Center(
+              child: FutureBuilder<PokeHub>(
+            future: getPokemonRequest(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return _mainWidgetGrid(orientation, snapshot.data);
+              } else if (snapshot.hasError) {
+                return Text("Internet error");
+              }
+              return CircularProgressIndicator();
+            },
+          )),
+        );
+      }),
+    );
+  }
+
+  Widget _mainWidgetGrid(Orientation orientation, PokeHub pokeHub) {
+    return GridView.count(
+      crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+      children: pokeHub.pokemon
+          .map((pokeHub) => Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PokeDetail(
+                                pokemon: pokeHub,
+                              )));
+                },
+                child: Hero(
+                  tag: pokeHub.img,
+                  child: Card(
+                    elevation: 3.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(pokeHub.img))),
                         ),
-                      )))
-                  .toList(),
-            ),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.cyan,
-          child: Icon(Icons.refresh)),
+                        Text(pokeHub.name,
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ),
+              )))
+          .toList(),
     );
   }
 }
